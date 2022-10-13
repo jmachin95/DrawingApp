2b6221b21b2093ae3b2ac44422dc92e0a9fb122a
Jack Machin <jackmachin95@gmail.com>
2022-10-13 11:14:42 +0100

    new grid line toggling

--- drawingApp.java
+++ drawingApp.java
@@ -1,115 +1,245 @@
+
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
-
-//Canvas class
-
-//End of Canvas class
+import javax.swing.event.*;
+import java.io.*;
 
 
-//Structure of GUI eg. Layout components and panels
-public class drawingApp extends JFrame{
+public class drawingApp extends JFrame
+{
+    // GUI Component dimentsions.
+    private final int CANVAS_INITIAL_WIDTH = 800;
+    private final int CANVAS_INITIAL_HEIGHT = 640;
+    private final int CONTROL_PANEL_WIDTH = 200;
+    private final int MESSAGE_AREA_HEIGHT = 100;
 
-    public drawingApp(){
-        JFrame frame = new JFrame();
-        frame.setPreferredSize(new Dimension(1200, 1100));
+    
+    // Drawing area class (inner class).
+    class Canvas extends JPanel
+    {
+        // Called every time there is a change in the canvas contents.
+        public void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);        
+            draw(g);
+        }
+    } // end inner class Canvas
+    
+    
+    private Canvas canvas;
+    
+    private JPanel controlPanel;
+    private JLabel coordinatesLabel;
+    private JRadioButton lineRadioButton, ovalRadioButton, rectangleRadioButton, freehandRadioButton;
+    private JSlider freehandSizeSlider;
+    private JCheckBox fineCheckBox, coarseCheckBox;
+    private JButton colourButton, clearButton, animateButton;
+    
+    private JTextArea messageArea;
+    
+    private JMenuBar menuBar;
 
-        frame.setLayout(new BorderLayout());
+   
+        
+    
+    /*****************************************************************
+     * 
+     * Constructor method starts here
+     *    ... and goes on for quite a few lines of code 
+     */
+    public drawingApp()
+    {
+        setTitle("Drawing Application (da1)");
+        setLayout(new BorderLayout());  // Layout manager for the frame.
+        
+        // Canvas
+        canvas = new Canvas();
+          canvas.setBorder(new TitledBorder(new EtchedBorder(), "Canvas"));
+          canvas.setPreferredSize(new Dimension(CANVAS_INITIAL_WIDTH, CANVAS_INITIAL_HEIGHT));
+          // next line changes the cursor's rendering whenever the mouse drifts onto the canvas
+          canvas.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
 
-        JMenuBar menuBar = new JMenuBar();
-        menuBar.setPreferredSize(new Dimension(1200, 30));
-        JMenu fileMenu = new JMenu("File");
-        JMenuItem fileSaveMenuItem = new JMenuItem("Save");
-        JMenuItem fileLoadMenuItem = new JMenuItem("Load");
-        JMenuItem fileExitMenuItem = new JMenuItem("Exit");
-        fileMenu.add(fileSaveMenuItem);
-        fileMenu.add(fileLoadMenuItem);
-        fileMenu.add(fileExitMenuItem);
-        menuBar.add(fileMenu);
+          canvas.addMouseMotionListener(new CanvasMouseMotionListener());
+        add(canvas, BorderLayout.CENTER);
 
-        JMenu helpMenu = new JMenu("Help");
-        JMenuItem helpAboutMenuItem = new JMenuItem("About");
-        helpMenu.add(helpAboutMenuItem);
-        menuBar.add(helpMenu);
-
-        frame.add(menuBar, BorderLayout.PAGE_START);
-
-        //Canvas
-        JPanel canvas = new JPanel();
-        canvas.setBorder(new TitledBorder(new EtchedBorder(), "Canvas Panel"));            
-        canvas.setPreferredSize(new Dimension(1000, 400));
-        frame.add(canvas, BorderLayout.CENTER);
-
-
-        //Control Panel 
-        JPanel controPanel = new JPanel();
-        controPanel.setBorder(new TitledBorder(new EtchedBorder(), "Control Panel"));            
-        controPanel.setPreferredSize(new Dimension(200, 1000));
-        frame.add(controPanel, BorderLayout.LINE_START);
-
-
-        //Start of control panel components. Update in practical
-        ButtonGroup group = new ButtonGroup();
-        JRadioButton line = new JRadioButton("Line ");
-        line.setSelected(true);
-        JRadioButton rectangle = new JRadioButton("Rectangle ");
-        JRadioButton oval = new JRadioButton("Oval ");
-        JRadioButton freehand = new JRadioButton("Freehand ");
-        group.add(line);
-        group.add(rectangle);
-        group.add(oval);
-        group.add(freehand);
-
-        controPanel.add(line);
-        controPanel.add(rectangle);
-        controPanel.add(oval);
-        controPanel.add(freehand);
-
-        //End of control panel components
-
-
-        //Message Area
-        JPanel messageArea = new JPanel();
-        messageArea.setBorder(new TitledBorder(new EtchedBorder(), "Message Area"));            
-        messageArea.setPreferredSize(new Dimension(1200, 250));
-        frame.add(messageArea, BorderLayout.PAGE_END);
-
-        JTextArea textArea = new JTextArea(
-        "This is an editable JTextArea. " +
-        "A text area is a \"plain\" text component, " +
-        "which means that although it can display text " +
-        "in any font, all of the text is in the same font."
-        );
-
-        textArea.setPreferredSize(new Dimension(300, 50));
-
-        messageArea.add(textArea);
+        // Menu bar
+        menuBar = new JMenuBar();
+          JMenu fileMenu = new JMenu("File");
+            JMenuItem fileSaveMenuItem = new JMenuItem("Save");
+            fileMenu.add(fileSaveMenuItem);
+            JMenuItem fileLoadMenuItem = new JMenuItem("Load");
+            fileMenu.add(fileLoadMenuItem);
+            fileMenu.addSeparator();
+            JMenuItem fileExitMenuItem = new JMenuItem("Exit");
+            fileMenu.add(fileExitMenuItem);
+          menuBar.add(fileMenu);
+          JMenu helpMenu = new JMenu("Help");
+            JMenuItem helpAboutMenuItem = new JMenuItem("About");
+            helpMenu.add(helpAboutMenuItem);
+          menuBar.add(helpMenu);
+        add(menuBar, BorderLayout.PAGE_START);
+        
+        // Control Panel
+        controlPanel = new JPanel();
+          controlPanel.setBorder(new TitledBorder(new EtchedBorder(), "Control Panel"));
+          controlPanel.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH, CANVAS_INITIAL_HEIGHT));
+          // the following two lines put the control panel in a scroll pane (nicer?).      
+          JScrollPane controlPanelScrollPane = new JScrollPane(controlPanel);
+          controlPanelScrollPane.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH + 30, CANVAS_INITIAL_HEIGHT));
+        add(controlPanelScrollPane, BorderLayout.LINE_START);        
 
         
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Control Panel contents are specified in the next section eg: 
+        //    mouse coords panel; 
+        //    shape tools panel; 
+        //    trace-slider panel; 
+        //    grid panel; 
+        //    colour choice panel; 
+        //    "clear" n "animate" buttons
         
-        frame.pack();
-        frame.setVisible(true);
+        // Mouse Coordinates panel
+        JPanel coordinatesPanel = new JPanel();
+          coordinatesPanel.setBorder(new TitledBorder(new EtchedBorder(), "Drawing Position"));
+          coordinatesPanel.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH - 20, 60));
+          coordinatesLabel = new JLabel();
+          // coordinatesLabel.setText("coords");
+          
+          coordinatesPanel.add(coordinatesLabel);
+        controlPanel.add(coordinatesPanel);
+        
+        // Drawing tools panel
+        JPanel drawingToolsPanel = new JPanel();
+          drawingToolsPanel.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH - 20, 140));
+          drawingToolsPanel.setLayout(new GridLayout(0, 1));
+          drawingToolsPanel.setBorder(new TitledBorder(new EtchedBorder(), "Drawing Tools"));
+          
+        controlPanel.add(drawingToolsPanel);
+        
+        // Freehand trace size slider
+        JPanel freehandSliderPanel = new JPanel();
+          freehandSliderPanel.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH - 20, 90));
+          drawingToolsPanel.setLayout(new GridLayout(0, 1));
+          freehandSliderPanel.setBorder(new TitledBorder(new EtchedBorder(), "Freehand Size"));
+        controlPanel.add(freehandSliderPanel);
+
+        // Grid Panel
+        JPanel gridPanel = new JPanel();
+          gridPanel.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH - 20, 80));
+          gridPanel.setLayout(new GridLayout(0, 1));
+          gridPanel.setBorder(new TitledBorder(new EtchedBorder(), "Grid"));
+
+          
+          fineCheckBox = new JCheckBox("Fine");
+          coarseCheckBox = new JCheckBox("Coarse");
+          gridPanel.add(fineCheckBox);
+          gridPanel.add(coarseCheckBox);
+
+          fineCheckBox.addChangeListener(changeListener);
+          coarseCheckBox.addChangeListener(changeListener);
+
+        controlPanel.add(gridPanel);
+        
+        // Colour Panel
+        JPanel colourPanel = new JPanel();
+          colourPanel.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH - 20, 90));
+          colourPanel.setBorder(new TitledBorder(new EtchedBorder(), "Colour"));
+          colourButton = new JButton();
+          colourButton.setPreferredSize(new Dimension(50, 50));
+          colourPanel.add(colourButton);
+        controlPanel.add(colourPanel);
+
+        // Clear button
+        clearButton = new JButton("Clear Canvas");
+          clearButton.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH - 20, 50));
+        controlPanel.add(clearButton);
+
+        // Animate button 
+        animateButton = new JButton("Animate");
+          animateButton.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH - 20, 50));
+        controlPanel.add(animateButton);
+        
+        // that completes the control panel section
+
+        
+        // Message area
+        messageArea = new JTextArea();
+        messageArea.setEditable(false);
+        messageArea.setBackground(canvas.getBackground());
+        JScrollPane textAreaScrollPane = new JScrollPane(messageArea);
+        textAreaScrollPane.setBorder(new TitledBorder(new EtchedBorder(), "Message Area"));
+        textAreaScrollPane.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH + CANVAS_INITIAL_WIDTH, MESSAGE_AREA_HEIGHT));
+        add(textAreaScrollPane, BorderLayout.PAGE_END);
+
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        pack();
+        setVisible(true);
+        
+        
+    }  // end of the NBDrawApp1 constructor method
+    
+    // Called by the canvas' paintComponent method
+    void draw(Graphics g)
+    {
+      int width = canvas.getWidth();
+      int height = canvas.getHeight();
+
+      if(fineCheckBox.isSelected()){
+        g.setColor(new Color(0.8F, 0.8F, 0.8F, 0.8F));
+        for(int i = 0; i < width; i+=10){
+          g.drawLine(i, 0, i, height);
+        }
+  
+        for(int k = height; k > 10; k-=10){
+          g.drawLine(0,k, width, k);
+        }
+      }
+
+      if(coarseCheckBox.isSelected()){
+        g.setColor(new Color(0.6F, 0.6F, 0.6F, 0.6F));
+        for(int i = 0; i < width; i+=50){
+          g.drawLine(i, 0, i, height);
+        }
+  
+        for(int k = height; k > 10; k-=50){
+          g.drawLine(0,k, width, k);
+        }
+      }
+      
+    } // end draw method   
+
+    ChangeListener changeListener = new ChangeListener() {
+      public void stateChanged(ChangeEvent e) {
+        repaint();
+      };
+    };
+
+
+    class CanvasMouseMotionListener implements MouseMotionListener{
+      
+      public void mouseMoved(MouseEvent event){
+        int mouseX = event.getX();
+        int mouseY = event.getY();
+        coordinatesLabel.setText("x: " + mouseX +  " " + "y: " + mouseY);
+      }
+
+      public void mouseDragged(MouseEvent event){
+        int mouseX = event.getX();
+        int mouseY = event.getY();
+        coordinatesLabel.setText("x: " + mouseX + " " + "y: " + mouseY);
+      }
 
     }
-    //End of structure class
+
+    
+    public static void main(String args[])
+    {
+        drawingApp NBDrawApp1Instance = new drawingApp();
+    } // end main method
 
 
-
-    //Listener classes
-
-    //End of Listener classes
-
-
-
-    //Draw method
-
-    //End of Draw method
-
-
-    //Main method
-    public static void main(String[] args) {
-        new drawingApp();
-    }
-    //End of main method
-}
+    
+} // end of NBDrawApp1 class
